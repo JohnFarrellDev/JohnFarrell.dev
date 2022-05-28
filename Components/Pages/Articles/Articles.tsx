7e91e77fa54c88f36adccee9604ae54ed447ae2d
JohnFarrellDev <johnfarrell@fastmail.com>
2022-05-28 23:27:08 +0100

    move article data into an object

--- Components/Pages/Articles/Articles.tsx
+++ Components/Pages/Articles/Articles.tsx
@@ -5,6 +5,25 @@ import { SEO } from '../../SEO'
 import { Title } from '../../Utilities/Title'
 import styles from './Articles.module.css'
 
+interface Article {
+  URI: string
+  name: string
+  createdAt: Date
+}
+
+const articles: Article[] = [
+  {
+    URI: '/articles/recruiter-challenge',
+    name: 'Recruiter Challenge (£250 up for grabs)',
+    createdAt: new Date('2022-05-28T11:10:19.558Z'),
+  },
+  {
+    URI: '/articles/userscript-udemy-copy-out-quizzes',
+    name: 'UserScripts to copy Quiz Questions and Answers from Udemy',
+    createdAt: new Date('2022-05-27T11:16:06.761Z'),
+  },
+]
+
 export const Articles = () => {
   return (
     <Layout>
@@ -18,16 +37,14 @@ export const Articles = () => {
           <div className="section-center">
             <h2 className={styles.year}>2022</h2>
             <ul>
-              <li>
-                <Link href="/articles/recruiter-challenge">
-                  Recruiter Challenge (£250 up for grabs)
-                </Link>
-              </li>
-              <li>
-                <Link href="/articles/userscript-udemy-copy-out-quizzes">
-                  UserScripts to copy Quiz Questions and Answers from Udemy
-                </Link>
-              </li>
+              {articles
+                .filter((article) => article.createdAt.getFullYear() === 2022)
+                .map((article) => (
+                  <li key={article.URI}>
+                    <Link href={article.URI}>{`${article.name}`}</Link>
+                    <span> - {article.createdAt.toLocaleDateString()}</span>
+                  </li>
+                ))}
             </ul>
           </div>
         </section>
