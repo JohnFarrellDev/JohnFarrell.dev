9d6e3cc484c651d90e28fc1b8b737383cf1d39ed
JohnFarrellDev <johnfarrell@fastmail.com>
2022-05-29 13:07:36 +0100

    init storybook

---
+++ Components/Utilities/Title/Title.stories.tsx
@@ -0,0 +1,12 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import { Title } from './Title'
+
+export default {
+  title: 'Utilities',
+  component: Title,
+} as ComponentMeta<typeof Title>
+
+const Template: ComponentStory<typeof Title> = (args) => <Title {...args} />
+
+export const Default = Template.bind({})
+Default.args = { title: 'template title' }
