6e28a12545d035a1fa4505532189feb5c386fdf6
JohnFarrellDev <johnfarrell@fastmail.com>
2022-06-04 23:14:22 +0100

    new page for article on react project structure

---
+++ pages/articles/preferred-react-project-structure/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next'
+import { ReactProjectStructure as ReactProjectStructureArticle } from '../../../Components/Pages/Articles/ReactProjectStructure'
+
+const ReactProjectStructure: NextPage = () => {
+  return <ReactProjectStructureArticle />
+}
+
+export default ReactProjectStructure
