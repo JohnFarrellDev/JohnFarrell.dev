130ec0cb8e39d52fc608072a7dedc68ceaba9285
John Farrell <johnfarrell@fastmail.com>
2022-08-28 23:25:27 +0100

    fix generate board by now having a shared reference bug making each row identical

--- Components/Projects/Minesweeper/functions/generateBoard.ts
+++ Components/Projects/Minesweeper/functions/generateBoard.ts
@@ -4,16 +4,22 @@ export const generateBoard = (
   rows: number,
   columns: number
 ): { board: Cell[][] } => {
-  const board: Cell[][] = new Array(rows).fill(
-    new Array(columns).fill(undefined).map((_, index) => ({
-      id: index,
-      isCovered: true,
-      isBomb: false,
-      isFlagged: false,
-      neighbors: [],
-      neighborBombs: 0,
-    }))
-  )
+  const board: Cell[][] = []
+  for (let i = 0; i < rows; i++) {
+    const row: Cell[] = []
+    for (let j = 0; j < columns; j++) {
+      row.push({
+        id: j + i * columns,
+        isCovered: true,
+        isBomb: false,
+        isFlagged: false,
+        neighbors: [],
+        neighborBombs: 0,
+      })
+    }
+
+    board.push(row)
+  }
 
   return { board }
 }
