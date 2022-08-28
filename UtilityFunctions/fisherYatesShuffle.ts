75a0b9acbc08bbd9f81710922012341315faadec
John Farrell <johnfarrell@fastmail.com>
2022-08-28 23:26:27 +0100

    utility function for shuffling array randomly

---
+++ UtilityFunctions/fisherYatesShuffle.ts
@@ -0,0 +1,14 @@
+export const fisherYatesShuffle = <T>(arr: T[]) => {
+  // fisher-yates random shuffling algorithm
+  let m = arr.length
+  while (m) {
+    // Pick a remaining element…
+    const i = Math.floor(Math.random() * m--)
+
+    // And swap it with the current element.
+    const t = arr[m]
+    arr[m] = arr[i]
+    arr[i] = t
+  }
+  return arr
+}
---
+++ UtilityFunctions/index.ts
@@ -0,0 +1,1 @@
+export { fisherYatesShuffle } from './fisherYatesShuffle'
