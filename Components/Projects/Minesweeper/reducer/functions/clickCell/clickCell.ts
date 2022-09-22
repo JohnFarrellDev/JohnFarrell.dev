357cb62456917ca932f3340fee5347461d5b2eb3
John Farrell <johnfarrell@fastmail.com>
2022-09-22 13:57:05 +0100

    click cell to do nothing if there are still animations to apply

--- Components/Projects/Minesweeper/reducer/functions/clickCell/clickCell.ts
+++ Components/Projects/Minesweeper/reducer/functions/clickCell/clickCell.ts
@@ -3,6 +3,7 @@ import { startGame } from './startGame'
 
 export const clickCell = (state: State, action: Action): State => {
   if (action.type !== 'ClickCell') return state
+  if(state.animationToApply.length > 0) return state
 
   if (!state.isPlaying) {
     return startGame(state, action)
