25e4c42effa52904ace85e9ec7cf6b769b688c93
John Farrell <johnfarrell@fastmail.com>
2022-09-22 14:01:53 +0100

    calculate neighbor information and number of bombs in a cell  with a borderless mode

--- Components/Projects/Minesweeper/reducer/functions/clickCell/calculateNeighborInformation.ts
+++ Components/Projects/Minesweeper/reducer/functions/clickCell/calculateNeighborInformation.ts
@@ -29,20 +29,61 @@ export const calculateNeighborInformation = (state: State, board: Cell[][]) => {
 
       const neighborCellsAnimation: Animation[] = []
 
-      neighborIndexes.forEach((neighborIndex) => {
-        if (
-          board[rowIndex + neighborIndex[0]]?.[columnIndex + neighborIndex[1]]
-        ) {
-          cellNeighbors.push(
-            board[rowIndex + neighborIndex[0]][columnIndex + neighborIndex[1]]
-          )
+      if (state.borderlessMode) {
+        const isOnLeft = columnIndex === 0
+        const isOnRight = columnIndex === state.columns - 1
+        const isOnTop = rowIndex === 0
+        const isOnBottom = rowIndex === state.rows - 1
+
+        const neighborIndexesBorderless = [
+          [isOnTop ? state.rows - 1 : -1, isOnLeft ? state.columns - 1 : -1],
+          [isOnTop ? state.rows - 1 : -1, 0],
+          [isOnTop ? state.rows - 1 : -1, isOnRight ? 0 : 1],
+          [0, isOnLeft ? state.columns - 1 : -1],
+          [0, isOnRight ? 0 : 1],
+          [isOnBottom ? 0 : 1, isOnLeft ? state.columns - 1 : -1],
+          [isOnBottom ? 0 : 1, 0],
+          [isOnBottom ? 0 : 1, isOnRight ? 0 : 1],
+        ]
+
+        neighborIndexesBorderless.forEach((neighborIndex, indx) => {
+          const nRowIndex =
+            isOnTop && indx <= 2
+              ? state.rows - 1
+              : isOnBottom && indx >= 5
+              ? 0
+              : rowIndex + neighborIndex[0]
+
+          const nColumnIndex =
+            isOnLeft && (indx === 0 || indx === 3 || indx === 5)
+              ? state.columns - 1
+              : isOnRight && (indx === 2 || indx === 4 ||indx === 7)
+              ? 0
+              : columnIndex + neighborIndex[1]
+
+          cellNeighbors.push(board[nRowIndex][nColumnIndex])
           neighborCellsAnimation.push({
-            rowIndex: rowIndex + neighborIndex[0],
-            columnIndex: columnIndex + neighborIndex[1],
+            rowIndex: nRowIndex,
+            columnIndex: nColumnIndex,
             color: '#ff3399',
           })
-        }
-      })
+        })
+      } else {
+        neighborIndexes.forEach((neighborIndex) => {
+          if (
+            board[rowIndex + neighborIndex[0]]?.[columnIndex + neighborIndex[1]]
+          ) {
+            cellNeighbors.push(
+              board[rowIndex + neighborIndex[0]][columnIndex + neighborIndex[1]]
+            )
+            neighborCellsAnimation.push({
+              rowIndex: rowIndex + neighborIndex[0],
+              columnIndex: columnIndex + neighborIndex[1],
+              color: '#ff3399',
+            })
+          }
+        })
+      }
 
       if (state.customAnimations.get('CalculateNeighbors')) {
         state.animationToApply.push({
@@ -52,7 +93,7 @@ export const calculateNeighborInformation = (state: State, board: Cell[][]) => {
 
         state.animationToApply.push({
           time: 400,
-          animations: "WIPE"
+          animations: 'WIPE',
         })
       }
 
