2502d523cd38520bd9dcccb49db9cb78476ddd7f
John Farrell <johnfarrell@fastmail.com>
2022-09-25 17:23:13 +0100

    make clickCell statefull

--- Components/Projects/Minesweeper/reducer/functions/clickCell/clickCell.ts
+++ Components/Projects/Minesweeper/reducer/functions/clickCell/clickCell.ts
@@ -2,13 +2,11 @@ import { Action, State } from '../..'
 import { revealCell } from './revealCell'
 import { startGame } from './startGame'
 
-export const clickCell = (state: State, action: Action): State => {
+export const clickCell = (state: State, action: Action) => {
   if (action.type !== 'ClickCell') return state
   if(state.animationToApply.length > 0) return state
 
-  if (!state.isPlaying || state.isDead || state.isWinner) {
-    return startGame(state, action)
-  }
+  if (!state.isPlaying || state.isDead || state.isWinner) return startGame(state, action)
 
-  return revealCell(state, action)
+  revealCell(state, action)
 }
