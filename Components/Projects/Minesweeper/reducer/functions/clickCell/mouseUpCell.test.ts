48eefedd45f3b4397f1443b34394f01f7bae548b
John Farrell <johnfarrell@fastmail.com>
2022-09-25 21:38:58 +0100

    test mouse up cell

---
+++ Components/Projects/Minesweeper/reducer/functions/clickCell/mouseUpCell.test.ts
@@ -0,0 +1,78 @@
+import { State } from '../..'
+import { FaceType } from '../../../Components/GameTracking/GameTracking'
+import { generateBoard } from '../../../functions/generateBoard'
+import { mouseUpCell } from './mouseUpCell'
+
+const startingState: State = {
+  animationToApply: [],
+  animationTime: 0,
+  columns: 5,
+  rows: 5,
+  customAnimations: {
+    CalculateNeighbors: true,
+    PlaceBombs: false,
+  },
+  allowedOperations: {
+    CalculateNeighbors: true,
+    FlagCell: false,
+    PlaceBombs: false,
+    RevealCell: false,
+  },
+  isDead: false,
+  isPlaying: true,
+  isWinner: false,
+  numberOfBombs: 5,
+  borderlessMode: false,
+  board: [],
+  isHoldingDown: true,
+  faceType: FaceType.Human,
+}
+
+describe('mouse up cell', () => {
+  let state = { ...startingState }
+
+  beforeEach(() => {
+    state = {
+      ...startingState,
+      animationToApply: [],
+      customAnimations: { ...startingState.customAnimations },
+      allowedOperations: { ...startingState.allowedOperations },
+    }
+    generateBoard(state)
+  })
+
+  it('should do nothing if isPLaying is false', () => {
+    state.isPlaying = false;
+    expect(state.isHoldingDown).toBe(true)
+    
+    mouseUpCell(state)
+
+    expect(state.isHoldingDown).toBe(true)
+  })
+
+  it('should do nothing if isDead is true', () => {
+    state.isDead = true;
+    expect(state.isHoldingDown).toBe(true)
+    
+    mouseUpCell(state)
+
+    expect(state.isHoldingDown).toBe(true)
+  })
+
+  it('should do nothing if isWinner is true', () => {
+    state.isWinner = true;
+    expect(state.isHoldingDown).toBe(true)
+    
+    mouseUpCell(state)
+
+    expect(state.isHoldingDown).toBe(true)
+  })
+
+  it('should set isHolding to true', () => {
+    expect(state.isHoldingDown).toBe(true)
+    
+    mouseUpCell(state)
+
+    expect(state.isHoldingDown).toBe(false)
+  })
+})
--- Components/Projects/Minesweeper/reducer/functions/clickCell/mouseUpCell.ts
+++ Components/Projects/Minesweeper/reducer/functions/clickCell/mouseUpCell.ts
@@ -1,7 +1,7 @@
 import { State } from '../..'
 
 export const mouseUpCell = (state: State) => {
-  if (!state.isPlaying) return
+    if (!state.isPlaying || state.isWinner || state.isDead) return
 
   state.isHoldingDown = false
 }
