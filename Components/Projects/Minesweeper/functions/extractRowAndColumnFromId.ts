6e26b27ce1cd26953ded6734800e0c23cb6a0127
John Farrell <johnfarrell@fastmail.com>
2022-09-25 01:52:29 +0100

    utility function, convert 1d position to 2d position

---
+++ Components/Projects/Minesweeper/functions/extractRowAndColumnFromId.ts
@@ -0,0 +1,9 @@
+export const extractRowAndColumnFromId = (
+  id: number,
+  numberOfColumns: number
+): [row: number, column: number] => {
+  const randomBombLocationColumn = id % numberOfColumns
+  const randomBombLocationRow = Math.floor(id / numberOfColumns)
+
+  return [randomBombLocationRow, randomBombLocationColumn]
+}
