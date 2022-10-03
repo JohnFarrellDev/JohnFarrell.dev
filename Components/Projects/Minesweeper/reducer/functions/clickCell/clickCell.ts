d5c01a279bf6a1a04b7b96e9cf58646f9b7331e1
John Farrell <johnfarrell@fastmail.com>
2022-10-03 02:04:24 +0100

    click to skip animation

--- Components/Projects/Minesweeper/reducer/functions/clickCell/clickCell.ts
+++ Components/Projects/Minesweeper/reducer/functions/clickCell/clickCell.ts
@@ -1,9 +1,14 @@
-import { ClickCellAction, State } from '../..'
+import { AnimationStep, ClickCellAction, State } from '../..'
+import { Queue } from '../../../../../../Utilities/Queue'
 import { revealCell } from './revealCell/revealCell'
 import { startGame } from './startGame'
 
 export const clickCell = (state: State, action: ClickCellAction) => {
-  if(state.animationToApply.length > 0) return
+  if(state.animationToApply.length > 0) {
+    // TODO - left click skip, right click to pause?
+    state.animationToApply = new Queue<AnimationStep>();
+    return
+  }
 
   if (!state.isPlaying || state.isDead || state.isWinner) return startGame(state, action)
 
