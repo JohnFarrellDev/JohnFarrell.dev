d21f5b4ddc0c665d2ee8797fc4e49ed6458ceb04
John Farrell <johnfarrell@fastmail.com>
2022-10-03 19:32:43 +0100

    add animation effect for basic cell auto reveal

--- Components/Projects/Minesweeper/reducer/functions/clickCell/revealCell/autoRevealCells.ts
+++ Components/Projects/Minesweeper/reducer/functions/clickCell/revealCell/autoRevealCells.ts
@@ -1,11 +1,12 @@
-import { State } from '../../..'
+import { AnimationColor, Animation, State } from '../../..'
+import { extractRowAndColumnFromId } from '../../../../functions/extractRowAndColumnFromId'
 import { Cell } from '../../../../types'
 
 const numberOfFlaggedNeighborCells = (cell: Cell): number =>
   cell.neighbors.reduce((prev, curr) => prev + Number(curr.isFlagged), 0)
 
 export const autoRevealCells = (state: State): boolean => {
-  if(!state.allowedOperations.BasicAutoClick) return false
+  if (!state.allowedOperations.BasicAutoClick) return false
 
   let scanAgain = false
 
@@ -16,19 +17,70 @@ export const autoRevealCells = (state: State): boolean => {
       const flaggedNeighbors = numberOfFlaggedNeighborCells(state.board[r][c])
 
       if (state.board[r][c].neighborBombs <= flaggedNeighbors) {
+        if (state.customAnimations.BasicAutoClick) {
+          state.animationToApply.enqueue({
+            time: 500,
+            animations: [
+              {
+                rowIndex: r,
+                columnIndex: c,
+                color: AnimationColor.SelectedCell,
+              },
+            ],
+          })
+        }
+
+        const neighborSelectedReveal: Animation[] = []
+
         state.board[r][c].neighbors.forEach((neighborCell) => {
           if (
             neighborCell.isCovered &&
             !neighborCell.isFlagged &&
             !neighborCell.isBomb
           ) {
-            neighborCell.isCovered = false;
-            scanAgain = true;
+            neighborCell.isCovered = false
+            scanAgain = true
+
+            if (state.customAnimations.BasicAutoClick) {
+              const [row, column] = extractRowAndColumnFromId(
+                neighborCell.id,
+                state.columns
+              )
+
+              neighborSelectedReveal.push({
+                columnIndex: column,
+                rowIndex: row,
+                color: AnimationColor.RecursiveRevealColor,
+              })
+            }
           }
         })
+
+        if (neighborSelectedReveal.length > 0) {
+          state.animationToApply.enqueue({
+            time: 500,
+            animations: [
+              {
+                rowIndex: r,
+                columnIndex: c,
+                color: AnimationColor.SelectedCell,
+              },
+            ],
+          })
+          state.animationToApply.enqueue({
+            time: 500,
+            animations: neighborSelectedReveal,
+          })
+        }
+        if (neighborSelectedReveal.length > 0) {
+          state.animationToApply.enqueue({
+            time: 200,
+            animations: 'WIPE',
+          })
+        }
       }
     }
   }
 
-  return scanAgain;
+  return scanAgain
 }
