21cdf02d364c739d18d624298817ac934c01325f
John Farrell <johnfarrell@fastmail.com>
2022-10-09 02:06:52 +0100

    auto reveal cell tests to work with apply changes

--- Components/Projects/Minesweeper/reducer/functions/clickCell/revealCell/autoRevealCell.test.ts
+++ Components/Projects/Minesweeper/reducer/functions/clickCell/revealCell/autoRevealCell.test.ts
@@ -1,4 +1,4 @@
-import { AnimationColor, State } from '../../..'
+import { State } from '../../..'
 import { minesweeperStateFactory } from '../../../../../../../factories/minesweeperState'
 import { generateBoard } from '../../../../functions/generateBoard'
 import { calculateNeighborInformation } from '../calculateNeighborInformation'
@@ -40,7 +40,7 @@ describe('auto reveal cell', () => {
   })
 
   it('should do nothing if the number of flagged neighbor cells is less than the number of neighbor bombs', () => {
-    state.board[1][1].neighborBombs = 1
+    state.revealedBoard[1][1].neighborBombs = 1
 
     const repeat = autoRevealCells(state)
 
@@ -48,14 +48,14 @@ describe('auto reveal cell', () => {
   })
 
   it('should uncover all non flagged covered neighbor cells if the number of flags equals number of neighbor bombs', () => {
-    state.board[0][0].isFlagged = true
-    state.board[1][1].neighborBombs = 1
-    state.board[1][1].isCovered = false
+    state.revealedBoard[0][0].isFlagged = true
+    state.revealedBoard[1][1].neighborBombs = 1
+    state.revealedBoard[1][1].isCovered = false
 
     for (let r = 0; r < state.rows; r++) {
       for (let c = 0; c < state.columns; c++) {
         if (r === 1 && c === 1) continue
-        expect(state.board[r][c].isCovered).toBe(true)
+        expect(state.revealedBoard[r][c].isCovered).toBe(true)
       }
     }
 
@@ -65,24 +65,24 @@ describe('auto reveal cell', () => {
     for (let r = 0; r < state.rows; r++) {
       for (let c = 0; c < state.columns; c++) {
         if (r === 0 && c === 0) {
-          expect(state.board[r][c].isCovered).toBe(true)
+          expect(state.revealedBoard[r][c].isCovered).toBe(true)
         } else {
-          expect(state.board[r][c].isCovered).toBe(false)
+          expect(state.revealedBoard[r][c].isCovered).toBe(false)
         }
       }
     }
   })
 
   it('should never automatically reveal a cell that is a bomb due to bad user flagging', () => {
-    state.board[0][0].isFlagged = true
-    state.board[1][1].neighborBombs = 1
-    state.board[1][1].isCovered = false
-    state.board[0][1].isBomb = true
+    state.revealedBoard[0][0].isFlagged = true
+    state.revealedBoard[1][1].neighborBombs = 1
+    state.revealedBoard[1][1].isCovered = false
+    state.revealedBoard[0][1].isBomb = true
 
     for (let r = 0; r < state.rows; r++) {
       for (let c = 0; c < state.columns; c++) {
         if (r === 1 && c === 1) continue
-        expect(state.board[r][c].isCovered).toBe(true)
+        expect(state.revealedBoard[r][c].isCovered).toBe(true)
       }
     }
 
@@ -92,76 +92,112 @@ describe('auto reveal cell', () => {
     for (let r = 0; r < state.rows; r++) {
       for (let c = 0; c < state.columns; c++) {
         if ((r === 0 && c === 0) || (r === 0 && c === 1)) {
-          expect(state.board[r][c].isCovered).toBe(true)
+          expect(state.revealedBoard[r][c].isCovered).toBe(true)
         } else {
-          expect(state.board[r][c].isCovered).toBe(false)
+          expect(state.revealedBoard[r][c].isCovered).toBe(false)
         }
       }
     }
   })
 
-  it('should apply no animations if there are no cells automatically revealed', () => {
+  it('should apply no changes if there are no cells automatically revealed', () => {
     state.customAnimations.BasicAutoClick = true
-    state.board[1][1].neighborBombs = 1
+    state.revealedBoard[1][1].neighborBombs = 1
 
     autoRevealCells(state)
 
-    expect(state.changesToApply.length).toBe(0)
+    const changesToApply = state.changesToApply.toArray()
+
+    expect(changesToApply.length).toBe(1)
+    expect(changesToApply[0].changes[0].action).toBe('COPYNEIGHBORBOMBCOUNT')
   })
 
-  it('should apply animations when cells are automatically revealed and custom animation is turned on', () => {
-    state.customAnimations.BasicAutoClick = true
-    state.board[0][0].isFlagged = true
-    state.board[1][1].neighborBombs = 1
-    state.board[1][1].isCovered = false
-    state.board[0][1].isBomb = true
+  it('should apply changes with no animation when cells are automatically revealed and custom animation is turned off', () => {
+    state.revealedBoard[0][0].isFlagged = true
+    state.revealedBoard[1][1].neighborBombs = 1
+    state.revealedBoard[1][1].isCovered = false
+    state.revealedBoard[0][1].isBomb = true
 
     autoRevealCells(state)
 
     const animations = state.changesToApply.toArray()
-    
-    expect(animations.length).toBe(3)
+
+    expect(animations.length).toBe(2)
+
     expect(animations[0]).toEqual({
-      time: 500,
+      time: 0,
+      changes: [{ action: 'COPYNEIGHBORBOMBCOUNT' }],
+    })
+
+    expect(animations[1]).toEqual({
+      time: 0,
       changes: [
-        { rowIndex: 1, columnIndex: 1, color: AnimationColor.SelectedCell },
+        {
+          action: "REVEALCELLS",
+          cells: [
+            {
+              rowIndex: 0,
+              columnIndex: 2,
+            },
+            {
+              rowIndex: 1,
+              columnIndex: 0,
+            },
+            {
+              rowIndex: 1,
+              columnIndex: 2,
+            },
+            {
+              rowIndex: 2,
+              columnIndex: 0,
+            },
+            {
+              rowIndex: 2,
+              columnIndex: 1,
+            },
+            {
+              rowIndex: 2,
+              columnIndex: 2,
+            },
+          ],
+        },
       ],
     })
+  })
+
+  it('should apply changes with animations when cells are automatically revealed and custom animation is turned on', () => {
+    state.customAnimations.BasicAutoClick = true
+    state.revealedBoard[0][0].isFlagged = true
+    state.revealedBoard[1][1].neighborBombs = 1
+    state.revealedBoard[1][1].isCovered = false
+    state.revealedBoard[0][1].isBomb = true
+
+    autoRevealCells(state)
+
+    const animations = state.changesToApply.toArray()
+
+    expect(animations.length).toBe(9)
+
+    expect(animations[0]).toEqual({
+      time: 0,
+      changes: [{ action: 'COPYNEIGHBORBOMBCOUNT' }],
+    })
+
     expect(animations[1]).toEqual({
       time: 500,
+      changes: [{ action: 'SELECTEDCELL', rowIndex: 1, columnIndex: 1 }],
+    })
+    expect(animations[2]).toEqual({
+      time: 500,
+      changes: [{ action: 'REVEALCELLANIMATED', rowIndex: 0, columnIndex: 2 }],
+    })
+    expect(animations[animations.length - 1]).toEqual({
+      time: 500,
       changes: [
         {
-          rowIndex: 0,
-          columnIndex: 2,
-          color: AnimationColor.RecursiveRevealColor,
-        },
-        {
-          rowIndex: 1,
-          columnIndex: 0,
-          color: AnimationColor.RecursiveRevealColor,
-        },
-        {
-          rowIndex: 1,
-          columnIndex: 2,
-          color: AnimationColor.RecursiveRevealColor,
-        },
-        {
-          rowIndex: 2,
-          columnIndex: 0,
-          color: AnimationColor.RecursiveRevealColor,
-        },
-        {
-          rowIndex: 2,
-          columnIndex: 1,
-          color: AnimationColor.RecursiveRevealColor,
-        },
-        {
-          rowIndex: 2,
-          columnIndex: 2,
-          color: AnimationColor.RecursiveRevealColor,
+          action: 'WIPEANIMATION',
         },
       ],
     })
-    expect(animations[2]).toEqual({time: 200, animations: "WIPE"})
   })
 })
