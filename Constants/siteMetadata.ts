6eaccd0b853899a7ce9fd41857efa6ba9431249e
John Farrell <johnfarrell@fastmail.com>
2023-12-07 20:13:40 +0000

    smaller default og image

--- Constants/siteMetadata.ts
+++ Constants/siteMetadata.ts
@@ -2,5 +2,5 @@ export const siteMetadata = {
   title: 'John Farrell | Professional Software Engineer',
   description: 'My website for sharing projects and articles.',
   twitterUsername: 'JohnFarrellDev',
-  image: 'https://i.imgur.com/ApkQdnk.png',
+  image: 'https://i.imgur.com/CoW8DSQ.png',
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
-import type { NextPage } from 'next'
 import { Layout } from '../Components/Layout/Layout'
 import { SEO } from '../Components/SEO/SEO'
 import { Hero } from '../Components/Hero/Hero'
 import { Skills } from '../Components/Skills/Skills'
 import { Experience } from '../Components/Experience/Experience'
 
-const Home: NextPage = () => {
+const Home = () => {
   return (
     <Layout>
       <SEO />
