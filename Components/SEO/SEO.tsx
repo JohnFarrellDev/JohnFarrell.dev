ea95bdf0f1690349980c85499f3eecfcb9ebda6f
John Farrell <johnfarrell@fastmail.com>
2023-12-08 02:21:14 +0000

    move metadata into SEO module

--- Components/SEO/SEO.tsx
+++ Components/SEO/SEO.tsx
@@ -1,6 +1,11 @@
 import Head from 'next/head'
-import React from 'react'
-import { siteMetadata } from '../../Constants/siteMetadata'
+
+const siteMetadata = {
+  title: 'John Farrell | Professional Software Engineer',
+  description: 'My website for sharing projects and articles.',
+  twitterUsername: 'JohnFarrellDev',
+  image: 'https://i.imgur.com/CoW8DSQ.png',
+}
 
 interface SEOProps {
   title?: string
@@ -12,25 +17,16 @@ export const SEO = ({ title, description, image }: SEOProps) => {
   return (
     <Head>
       <title>{title || siteMetadata.title}</title>
-      <meta
-        name="description"
-        content={description || siteMetadata.description}
-      />
+      <meta name="description" content={description || siteMetadata.description} />
 
       <meta property="og:title" content={title || siteMetadata.title} />
       <meta property="og:url" content="https://john-farrell.dev/" />
       <meta property="og:type" content="website" />
-      <meta
-        property="og:description"
-        content={description || siteMetadata.description}
-      />
+      <meta property="og:description" content={description || siteMetadata.description} />
       <meta property="og:image" content={image || siteMetadata.image} />
 
       <meta property="twitter:title" content={title || siteMetadata.title} />
-      <meta
-        property="twitter:description"
-        content={description || siteMetadata.description}
-      />
+      <meta property="twitter:description" content={description || siteMetadata.description} />
       <meta property="twitter:image" content={image || siteMetadata.image} />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content={siteMetadata.twitterUsername} />
