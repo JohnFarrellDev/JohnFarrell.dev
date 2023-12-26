9c9aad776b5a6e8771293f26afba1bec0092a03e
John Farrell <johnfarrell@fastmail.com>
2023-12-26 23:06:00 +0000

    hydrate level mode so correct number of slots shown

--- Components/Projects/DescendingInsanity/GameWithLevels.tsx
+++ Components/Projects/DescendingInsanity/GameWithLevels.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import { UseLocalStorage } from '../../../Utilities/UseLocalStorage'
 import { DescendingNumberGame } from './DescendingNumberGame'
 
@@ -7,6 +8,14 @@ export const GameWithLevels = () => {
     setValueLocalStorageNoRerender: setLevel,
     refetch,
   } = UseLocalStorage('descending-insanity-level', 1)
+
+  const [hydration, setHydration] = useState(false)
+
+  useEffect(() => {
+    setHydration(true)
+  }, [])
+
+  if (!hydration) return null
 
   const numberOfSlots = level + 4
 
