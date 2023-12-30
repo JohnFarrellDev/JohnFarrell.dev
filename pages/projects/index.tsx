07dc1652021cc1b5e02337ade8b9c0150ae33386
John Farrell <johnfarrell@fastmail.com>
2023-12-30 16:23:40 +0000

    fix project page card spacing

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,46 +1,47 @@
+import { Fragment } from 'react'
 import { ArticleCard } from '../../Components/Articles/ArticleCard'
 import { Layout } from '../../Components/Layout/Layout'
 import { SEO } from '../../Components/SEO/SEO'
 import { Title } from '../../Components/Utilities/Title/Title'
 import styles from './projects.module.css'
 
-interface Project {
-  title: string
-  description: string
-  URL: string
-  imageURL: string
-  imageAlt: string
-  createdAt: Date
-  tags: string[]
-}
-
-const projects: Project[] = [
+const allProjects = [
   {
-    title: 'Descending Insanity',
-    description: 'How far can you go?',
-    URL: '/projects/descending-insanity',
-    imageURL: 'https://i.imgur.com/kp6xRVV.png',
-    imageAlt: '',
-    createdAt: new Date('2023-12-26T20:00:00.000Z'),
-    tags: [],
+    year: '2023',
+    projects: [
+      {
+        title: 'Descending Insanity',
+        description: 'How far can you go?',
+        URL: '/projects/descending-insanity',
+        imageURL: 'https://i.imgur.com/kp6xRVV.png',
+        imageAlt: '',
+        createdAt: new Date('2023-12-26T20:00:00.000Z'),
+        tags: [],
+      },
+      {
+        title: 'Twenty Number Challenge',
+        description: 'A simple luck based number game',
+        URL: '/projects/20-number-challenge',
+        imageURL: 'https://i.imgur.com/kp6xRVV.png',
+        imageAlt: '',
+        createdAt: new Date('2023-12-24T15:00:00.000Z'),
+        tags: [],
+      },
+    ],
   },
   {
-    title: 'Twenty Number Challenge',
-    description: 'A simple luck based number game',
-    URL: '/projects/20-number-challenge',
-    imageURL: 'https://i.imgur.com/kp6xRVV.png',
-    imageAlt: '',
-    createdAt: new Date('2023-12-24T15:00:00.000Z'),
-    tags: [],
-  },
-  {
-    title: 'Minesweeper solved and visualised',
-    description: "Implementing minesweeper and it's automated solving algorithms visualised.",
-    URL: '/projects/minesweeper',
-    imageURL: 'https://i.imgur.com/OjqgY02.jpg',
-    imageAlt: '',
-    createdAt: new Date('2022-10-09T22:19:37.934Z'),
-    tags: [],
+    year: '2022',
+    projects: [
+      {
+        title: 'Minesweeper solved and visualised',
+        description: "Implementing minesweeper and it's automated solving algorithms visualised.",
+        URL: '/projects/minesweeper',
+        imageURL: 'https://i.imgur.com/OjqgY02.jpg',
+        imageAlt: '',
+        createdAt: new Date('2022-10-09T22:19:37.934Z'),
+        tags: [],
+      },
+    ],
   },
 ]
 
@@ -51,40 +52,16 @@ const Projects = () => {
       <section className="section projects">
         <Title title="Projects" />
         <div className="section-center projects-center">
-          <h2 className={styles.year}>2023</h2>
-          <ul>
-            {projects
-              .filter((project) => project.createdAt.getFullYear() === 2023)
-              .map((project) => (
-                <ArticleCard
-                  title={project.title}
-                  description={project.description}
-                  URL={project.URL}
-                  createdAt={project.createdAt}
-                  imageURL={project.imageURL}
-                  imageAlt={project.imageAlt}
-                  tags={[]}
-                  key={project.title}
-                />
-              ))}
-          </ul>
-          <h2 className={styles.year}>2022</h2>
-          <ul>
-            {projects
-              .filter((project) => project.createdAt.getFullYear() === 2022)
-              .map((project) => (
-                <ArticleCard
-                  title={project.title}
-                  description={project.description}
-                  URL={project.URL}
-                  createdAt={project.createdAt}
-                  imageURL={project.imageURL}
-                  imageAlt={project.imageAlt}
-                  tags={[]}
-                  key={project.title}
-                />
-              ))}
-          </ul>
+          {allProjects.map(({ year, projects }) => (
+            <Fragment key={year}>
+              <h2 className={styles.year}>{year}</h2>
+              <ul className={styles.articlesContainer}>
+                {projects.map((project) => (
+                  <ArticleCard key={project.URL} {...project} />
+                ))}
+              </ul>
+            </Fragment>
+          ))}
         </div>
       </section>
     </Layout>
