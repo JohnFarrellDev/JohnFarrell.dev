5e93693c67348e487abc8bd9f98da38e5e020e9c
John Farrell <johnfarrell@fastmail.com>
2024-06-24 20:21:32 +0100

    remove recruiter challenge article

--- pages/articles/recruiter-challenge/index.tsx
+++
@@ -1,8 +0,0 @@
-import type { NextPage } from 'next'
-import { RecruiterChallenge as RecruiterChallengeArticle } from '../../../Components/Articles/RecruiterChallenge/RecruiterChallenge'
-
-const RecruiterChallenge: NextPage = () => {
-  return <RecruiterChallengeArticle />
-}
-
-export default RecruiterChallenge
