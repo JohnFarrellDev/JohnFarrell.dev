01e8c75291c32905d521db55d6c5b440183f1b8d
John Farrell <johnfarrell@fastmail.com>
2024-06-24 20:38:50 +0100

    remove another pointless folder

---
+++ pages/articles/minesweeper.tsx
@@ -0,0 +1,7 @@
+import { Minesweeper as MinesweeperArticle } from '../../Components/Articles/Minesweeper/Minesweeper'
+
+const Minesweeper = () => {
+  return <MinesweeperArticle />
+}
+
+export default Minesweeper
--- pages/articles/minesweeper/index.tsx
+++
@@ -1,7 +0,0 @@
-import { Minesweeper as MinesweeperArticle } from '../../../Components/Articles/Minesweeper/Minesweeper'
-
-const Minesweeper = () => {
-  return <MinesweeperArticle />
-}
-
-export default Minesweeper
