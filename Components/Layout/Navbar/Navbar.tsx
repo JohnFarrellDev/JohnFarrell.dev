b76affe7342f6cdb1be2c9b5aad4bf756dd47b82
John Farrell <johnfarrell@fastmail.com>
2024-07-07 20:41:43 +0100

    fix overflow issues

--- Components/Layout/Navbar/Navbar.tsx
+++ Components/Layout/Navbar/Navbar.tsx
@@ -2,8 +2,8 @@ import { Links } from '../Links/Links'
 
 export const Navbar = () => {
   return (
-    <nav className={'flex h-20 items-center bg-white'}>
-      <div className="mx-auto w-[90vw] max-w-[1170px]">
+    <nav className="flex h-20 items-center bg-white">
+      <div className="mx-auto max-w-[1170px]">
         <Links />
       </div>
     </nav>
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -6,8 +6,8 @@ const Contact = () => {
   return (
     <Layout>
       <SEO title="Contact Me | John Farrell" description="Form for contacting John Farrell" />
-      <section className={`grid h-full max-h-full min-h-full place-items-center bg-grey-1000`}>
-        <article className={'max-w[90vw] w-[35rem] rounded-lg bg-white text-center'}>
+      <section className="grid h-full max-h-full min-h-full max-w-full place-items-center bg-grey-1000">
+        <article className="max-w-full rounded-lg bg-white text-center">
           <h3 className="pt-4 text-grey-500">get in touch</h3>
           <ContactForm />
         </article>
