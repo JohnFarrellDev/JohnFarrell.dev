75a3286ac693bf5174ca5ea47ac136c12913fd2a
John Farrell <johnfarrell@fastmail.com>
2024-09-03 19:45:42 +0100

    tailwind code block

--- Components/Articles/Utilities/CodeBlock/CodeBlock.tsx
+++ Components/Articles/Utilities/CodeBlock/CodeBlock.tsx
@@ -2,26 +2,27 @@ import Link from 'next/link'
 import React, { useState } from 'react'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { a11yDark } from 'react-syntax-highlighter/dist/cjs/styles/prism'
-import styles from './CodeBlock.module.css'
+import { ExternalLink } from 'lucide-react'
 
 interface CodeBlockProps {
   canHide: boolean
   githubLink?: string
   children: string
-  fileName?: string
 }
 
-export const CodeBlock = ({ canHide, githubLink, children, fileName }: CodeBlockProps) => {
+export const CodeBlock = ({ canHide, githubLink, children }: CodeBlockProps) => {
   const [showCode, setShowCode] = useState(true)
 
   return (
     <>
-      <div className={styles.container}>
-        {githubLink && <Link href={githubLink}>Code on GitHub</Link>}
+      <div className="flex gap-4">
+        {githubLink && (
+          <Link href={githubLink} className="flex gap-2 text-link hover:text-link-hover">
+            <ExternalLink /> Code on GitHub
+          </Link>
+        )}
 
         {canHide && <button onClick={() => setShowCode(!showCode)}>{showCode ? 'Hide code' : 'Show code'}</button>}
-
-        {showCode && fileName && <p className={styles.fileName}>{fileName}</p>}
       </div>
       {showCode && (
         <SyntaxHighlighter
