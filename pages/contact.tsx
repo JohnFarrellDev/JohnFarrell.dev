959b3bdd91e393dbf36dface2ee74e0d67b90074
John Farrell <johnfarrell@fastmail.com>
2024-09-05 23:35:25 +0100

    fix contact form width

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -6,7 +6,7 @@ export default function Contact() {
   return (
     <Layout>
       <SEO title="Contact Me | John Farrell" description="Form for contacting John Farrell" />
-      <section className="grid h-full max-h-full min-h-full max-w-full place-items-center bg-grey-1000">
+      <section className="grid h-full max-h-full min-h-full max-w-full place-items-center bg-grey-1000 page-center">
         <article className="max-w-full rounded-lg bg-white text-center">
           <h3 className="pt-4 text-grey-500">get in touch</h3>
           <ContactForm />
