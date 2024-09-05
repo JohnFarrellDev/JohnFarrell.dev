180b138548fa93bc9bf2baf274e85b949fad6f92
John Farrell <johnfarrell@fastmail.com>
2024-09-05 23:24:50 +0100

    app router home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-import { Layout } from '../Components/Layout/Layout';
+'use client';
+
 import { SEO } from '../Components/SEO';
 import { Hero } from '../Components/Hero';
 import { Skills } from '../Components/Skills/Skills';
@@ -6,11 +7,11 @@ import { Experience } from '../Components/Experience/Experience';
 
 export default function Home() {
   return (
-    <Layout className="bg-primary-1000">
+    <div className="bg-primary-1000 pt-20">
       <SEO />
       <Hero />
       <Skills />
       <Experience />
-    </Layout>
+    </div>
   );
 }
