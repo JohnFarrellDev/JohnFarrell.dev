bd9c0a711f6fcb7b858767f975c9627986cff3fb
John Farrell <johnfarrell@fastmail.com>
2024-09-05 23:59:19 +0100

    fix metadata on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
-'use client';
-
-import { SEO } from '../Components/SEO';
 import { Hero } from '../Components/Hero';
 import { Skills } from '../Components/Skills/Skills';
 import { Experience } from '../Components/Experience/Experience';
+import { produceMetaData } from '../Utilities/produceMetaData';
+
+export const metadata = produceMetaData({});
 
 export default function Home() {
   return (
     <div className="bg-primary-1000 pt-20">
-      <SEO />
       <Hero />
       <Skills />
       <Experience />
