e7ef818baefc0fcbd8eeb077779b2bae5edf72dd
John Farrell <johnfarrell@fastmail.com>
2024-09-10 20:01:53 +0100

    update expected text for test

--- Components/ArticleTimeStamps.test.tsx
+++ Components/ArticleTimeStamps.test.tsx
@@ -5,7 +5,7 @@ import { describe, it, expect } from 'vitest';
 describe('TableOfContents', () => {
   it('should only display created at when last updated not provided', () => {
     render(<ArticleTimeStamps createdAt={new Date('2022-05-29T19:28:53.185Z')} />);
-    const createdAt = screen.getByText('Created at: May 29, 2022');
+    const createdAt = screen.getByText('Published: May 29, 2022');
     const lastUpdated = screen.queryByText('Last Updated:');
 
     expect(createdAt).toBeInTheDocument();
@@ -19,7 +19,7 @@ describe('TableOfContents', () => {
         lastUpdated={new Date('2022-05-30T19:28:53.185Z')}
       />
     );
-    const createdAt = screen.getByText('Created at: May 29, 2022');
+    const createdAt = screen.getByText('Published: May 29, 2022');
     const lastUpdated = screen.getByText('Last Updated: May 30, 2022');
 
     expect(createdAt).toBeInTheDocument();
