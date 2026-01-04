522a901c06e1b665f72fda8738681ec6d5e60397
John Farrell <johnfarrell@fastmail.com>
2026-01-04 19:26:08 +0000

    remove absolute positioning from the nav bar component

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <body className="flex min-h-svh flex-col bg-gray-50">
       <Navbar className="bg-primary-1000" />
-      <main className="grow pt-14 md:pt-20">{children}</main>
+      <main className="grow">{children}</main>
       <Footer />
     </body>
   );
