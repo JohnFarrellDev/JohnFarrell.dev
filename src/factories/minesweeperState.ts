8aaf16fa5a76800155d2671ca9f7f651a311745b
John Farrell <johnfarrell@fastmail.com>
2026-01-05 00:02:57 +0000

    remove fishery from the application, use function with partial overrides instead

--- src/factories/minesweeperState.ts
+++ src/factories/minesweeperState.ts
@@ -1,37 +1,45 @@
-import { Factory } from 'fishery';
 import { FaceType } from '../Components/Projects/Minesweeper/Components/GameTracking/GameTracking';
 import { ChangeStep, State } from '../Components/Projects/Minesweeper/reducer';
 import { Queue } from '../Utilities/Queue/queue';
 
-export const minesweeperStateFactory = Factory.define<State>(() => ({
-  rows: 10,
-  columns: 10,
-  numberOfBombs: 5,
-  borderlessMode: false,
-  customAnimations: {
-    CalculateNeighbors: false,
-    PlaceBombs: false,
-    RecursiveReveal: false,
-    FlagCell: false,
-    BasicAutoClick: false,
-  },
-  allowedOperations: {
-    AutoFlag: false,
-    BasicAutoClick: false,
-    CalculateNeighbors: false,
-    FlagCell: false,
-    PlaceBombs: false,
-    RecursiveReveal: false,
-    RevealCell: false,
-  },
-  board: [],
-  revealedBoard: [],
-  isPlaying: false,
-  isDead: false,
-  isWinner: false,
-  isHoldingDown: false,
-  changesToApply: new Queue<ChangeStep>(),
-  changeTime: 0,
-  faceType: FaceType.Human,
-  flagsPlaced: 0,
-}));
+function getDefaultState(): State {
+  return {
+    rows: 10,
+    columns: 10,
+    numberOfBombs: 5,
+    borderlessMode: false,
+    customAnimations: {
+      CalculateNeighbors: false,
+      PlaceBombs: false,
+      RecursiveReveal: false,
+      FlagCell: false,
+      BasicAutoClick: false,
+    },
+    allowedOperations: {
+      AutoFlag: false,
+      BasicAutoClick: false,
+      CalculateNeighbors: false,
+      FlagCell: false,
+      PlaceBombs: false,
+      RecursiveReveal: false,
+      RevealCell: false,
+    },
+    board: [],
+    revealedBoard: [],
+    isPlaying: false,
+    isDead: false,
+    isWinner: false,
+    isHoldingDown: false,
+    changesToApply: new Queue<ChangeStep>(),
+    changeTime: 0,
+    faceType: FaceType.Human,
+    flagsPlaced: 0,
+  };
+}
+
+export const minesweeperStateFactory = {
+  build: (overrides?: Partial<State>): State => ({
+    ...getDefaultState(),
+    ...overrides,
+  }),
+};
