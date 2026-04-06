83e889a1b760e5fb1ded25ace2c95476d1d11342
John Farrell <johnfarrell@fastmail.com>
2026-04-06 02:15:47 +0100

    update imports

--- src/app/(default)/projects/cv-utility/page.tsx
+++ src/app/(default)/projects/cv-utility/page.tsx
@@ -1,5 +1,5 @@
-import { PageContentContainer } from '@/Components/Layout/PageContent';
-import { PageWidthContainer } from '@/Components/Layout/PageWidthContainer';
+import { PageContentContainer } from '@/Components/Layout/PageContent/PageContent';
+import { PageWidthContainer } from '@/Components/Layout/PageWidthContainer/PageWidthContainer';
 import { CvBase } from '@/Components/Projects/CvUtility/CvBase';
 import { produceMetaData } from '@/Utilities/produceMetaData';
 
