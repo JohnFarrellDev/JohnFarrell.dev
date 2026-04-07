aaa8fa8974664c1b1b8568a56aeb130ce2e37673
John Farrell <johnfarrell@fastmail.com>
2026-04-07 11:24:56 +0100

    navigation link to cv

--- src/Components/Layout/Links/Links.tsx
+++ src/Components/Layout/Links/Links.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 
+import { Document } from '@/Components/icons/Document';
 import { House } from '@/Components/icons/House';
 import { Mail } from '@/Components/icons/Mail';
 import { NewsPaper } from '@/Components/icons/NewsPaper';
@@ -34,6 +35,12 @@ const data = [
   },
   {
     id: 5,
+    text: 'CV',
+    icon: <Document />,
+    url: '/cv/',
+  },
+  {
+    id: 6,
     text: 'contact',
     icon: <Mail />,
     url: '/contact/',
