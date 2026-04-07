eccdf18922576dae15efd914c01b836d0ef07c54
John Farrell <johnfarrell@fastmail.com>
2026-04-07 15:08:31 +0100

    add link to book a chat with me as part of the hero banner

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -2,7 +2,6 @@ import Link from 'next/link';
 
 import { PageContentContainer } from '@/Components/Layout/PageContent/PageContent';
 import { PageWidthContainer } from '@/Components/Layout/PageWidthContainer/PageWidthContainer';
-
 import { Robot } from '@/Components/Robot/Robot';
 import { SocialLinks } from '@/Components/SocialLinks/SocialLinks';
 
@@ -13,12 +12,22 @@ export function Hero() {
         <div>
           <h1 className="text-7xl font-bold">Hello, I'm John</h1>
           <h2 className="text-lg text-gray-600">I'm a software developer with a focus on the web</h2>
-          <Link
-            href="/contact"
-            className="bg-primary-800 text-primary-100 hover:bg-primary-300 hover:text-primary-1000 mx-auto mt-5 block w-fit rounded-md px-3.5 py-3.5 font-bold tracking-[0.2rem] uppercase no-underline shadow-lg transition-all duration-500 md:mx-0 md:block"
-          >
-            Contact Me
-          </Link>
+          <div className="mx-auto mt-5 flex w-fit gap-4 md:mx-0">
+            <Link
+              href="/contact"
+              className="bg-primary-800 text-primary-100 hover:bg-primary-300 hover:text-primary-1000 block rounded-md px-3.5 py-3.5 font-bold tracking-[0.2rem] uppercase no-underline shadow-lg transition-all duration-500"
+            >
+              Contact Me
+            </Link>
+            <a
+              href="https://cal.com/johnfarrelldev"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="bg-primary-800 text-primary-100 hover:bg-primary-300 hover:text-primary-1000 block rounded-md px-3.5 py-3.5 font-bold tracking-[0.2rem] uppercase no-underline shadow-lg transition-all duration-500"
+            >
+              Book a Chat
+            </a>
+          </div>
           <SocialLinks className="flex w-full justify-center gap-4 md:justify-start" styleLinks="w-8" />
         </div>
         <div className="hidden md:block">
