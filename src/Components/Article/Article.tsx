36a2cba20fcea3742ffc8d1763c3c6fb610052cc
John Farrell <johnfarrell@fastmail.com>
2026-04-08 10:52:49 +0100

    allow multiple deapth toc + h2 done as part of mdx component

--- src/Components/Article/Article.tsx
+++ src/Components/Article/Article.tsx
@@ -6,7 +6,7 @@ type ArticleProps = {
 
 export function ArticleStyles({ children }: ArticleProps) {
   return (
-    <div className="[&>a]:block [&>figcaption]:max-w-[70ch] [&>h2]:mt-8 [&_h2]:p-0 [&>h2]:text-2xl md:[&>h2]:text-3xl [&>h3]:mt-8 [&_h3]:mb-2 [&>h3]:p-0 [&_h3]:text-xl md:[&_h3]:text-2xl [&_h4]:mt-8 [&_h5]:mt-8 [&_h6]:mt-8 [&_li]:max-w-[70ch] [&>p]:mt-5 [&>p]:max-w-[70ch] [&>p]:text-lg [&>p]:text-pretty [&>p]:text-gray-900">
+    <div className="[&>h3]:mt-8 [&_h3]:mb-2 [&>h3]:p-0 [&_h3]:text-xl md:[&_h3]:text-2xl [&_h4]:mt-8 [&_h5]:mt-8 [&_h6]:mt-8 [&_li]:max-w-[70ch]">
       {children}
     </div>
   );
